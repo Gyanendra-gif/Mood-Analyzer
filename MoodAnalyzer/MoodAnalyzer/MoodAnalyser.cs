850a07e6ffb190ff6055bac385830ab01947d956
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-12-01 19:29:22 +0530

    [Gyanendra] Add: A Program to Handle Exception for Empty Mood and throw MoodAnalyseException

--- MoodAnalyzer/MoodAnalyzer/MoodAnalyser.cs
+++ MoodAnalyzer/MoodAnalyzer/MoodAnalyser.cs
@@ -9,23 +9,21 @@ namespace MoodAnalyzer
         {
             this.message = Message;
         }
-        
-        public string AnalyzeMood(string message)
-        {
-            try
-            {
-                if (this.message.Equals(string.Empty))
-                    throw new CustomException(CustomException.ExceptionType.NULL_MESSAGE, "Mood Should Not be Null");
-                if (message.Contains("Sad"))
-                    return "Sad";
-                else
-                    return "Happy";
-            }
-            catch (NullReferenceException)
-            {
-                throw new CustomException(CustomException.ExceptionType.NULL_MESSAGE, "Mood Should Not be Null");
-            }
-        }
-
+         public string AnalyzeMood(string message)
+         {
+             try
+             {
+                 if (this.message.Equals(string.Empty))
+                     throw new CustomException(CustomException.ExceptionType.NULL_MESSAGE, "Mood Should Not be Null");
+                 if (message.Contains("Sad"))
+                     return "Sad";
+                 else
+                     return "Happy";
+             }
+             catch (NullReferenceException)
+             {
+                 throw new CustomException(CustomException.ExceptionType.NULL_MESSAGE, "Mood Should Not be Null");
+             }
+         }
     }
 }
